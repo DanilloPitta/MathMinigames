d75ef7aff2687fd851c63c1f779fa6c3e856fd96
Danillo Pitta <71229806+DanilloPitta@users.noreply.github.com>
2023-09-11 09:18:57 -0300

    começo tutorial mathvaults

---
+++ MiniGames/MathVaults/Scripts/angela_talk.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@onready var angela_talk_ui = preload("res://MiniGames/MathVaults/Scenes/angela_talk_ui.tscn")
+
+var tutorial_completed = false
+
+
+func _on_body_entered(body):
+	if body.name == "CharacterBody2D":
+		if !tutorial_completed:
+			angela_talk_ui.instantiate()
