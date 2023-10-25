f334f3421d72a6bdfc17ca4a99ec838e4c17d246
Danillo Pitta <danillopitta@outlook.com>
2023-10-24 21:21:45 -0300

    Arrumar animação plates

--- src/Scripts/level.gd
+++ src/Scripts/level.gd
@@ -9,7 +9,3 @@ func _ready():
 	var level_plate_ = level_plate_scene.instantiate()
 	level_plate_.plate_texture = level_plate
 	get_parent().add_child(level_plate_)
-	
-	await get_tree().create_timer(4.2).timeout
-	
-	level_plate_.queue_free()
--- src/Scripts/level_plate.gd
+++ src/Scripts/level_plate.gd
@@ -6,3 +6,11 @@ func _ready():
 	$Control/TextureRect.texture = load("res://src/Assets/Hud/Levels_Plates/" + plate_texture)
 	
 	$AnimationPlayer.play("fade_in")
+	
+	await get_tree().create_timer(4.2).timeout
+	
+	$AnimationPlayer.play("bounce_out")
+	
+	await $AnimationPlayer.animation_finished
+	
+	self.queue_free()
